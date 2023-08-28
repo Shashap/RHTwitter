3759c170d1ceb0a7b34857652641e3ac6e2b56ba
hadas <hadas.spier@post.runi.ac.il>
2023-08-28 22:53:36 +0300

    add save feature enable

--- screens/admin.js
+++ screens/admin.js
@@ -50,7 +50,7 @@ router.post('/users/delete', (req, res) => {
 
 
 router.post('/features', (req, res) => {
-  const { feed, feedFilter, feedSort, search } = req.body;
+  const { feed, feedFilter, feedSort, feedSave, search } = req.body;
   try {
     const configFilePath = path.join(__dirname, 'data\\config.json');
     const configFileContent = fs.readFileSync(configFilePath, 'utf8');
@@ -60,8 +60,8 @@ router.post('/features', (req, res) => {
     config.features.feed = feed;
     config.features.feedFilter = feedFilter;
     config.features.feedSort = feedSort;
+    config.features.feedSave = feedSave;
     config.features.search = search;
-
     fs.writeFileSync(configFilePath, JSON.stringify(config, null, 2));
 
     res.json({ message: 'Feature status updated successfully.' });
